38d392bf3dbc8d7304161484066d7d5f6fa2f3aa
shubham suman <shubhamsuman653@gmail.com>
2025-03-24 20:56:42 +0530

    change is import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from './pages/home'
+import Home from './pages/Home'
 import About from './pages/About'
 
 
